3f8126ae4804f9cdff1e08e09eb6038fe01433a3
MhaWTHoR <e.kartal115@gmail.com>
2017-08-16 12:16:08 +0300

    CS:GO CASE CHANCE CALCULATOR

---
+++ CaseChance/src/com/mhawthor/casechance/Info.java
@@ -0,0 +1,14 @@
+package com.mhawthor.casechance;
+
+public final class Info {
+	
+	
+	
+	
+	
+	
+	public static final String PROGRAM_DEVELOPER = "Emirhan_Kartal";
+	public static final float VERSION = (float) 0.1;
+	public static final String PROGRAM_NAME = "CaseChanceCalculator";
+
+}
